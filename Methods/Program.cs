c38aa5fbe71430a79424449ea6095c53d9f83ae0
Murat ahmet Kıroğlu <mrt_drncli4161@outlook.com>
2023-04-27 22:29:21 +0300

    Update

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,22 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Add();
-            Add();
 
-            var result = Add2();
-            Console.WriteLine(result);
+            //Add();
+            //Add();
+
+            //var result = Add2();
+            //Console.WriteLine(result);
 
 
             int number1 = 20;
             int number2 = 100;
-            var result2 = Add3(ref number1, number2);
+            
+            var sonuc = Add3(  number1, number2);
 
-            Console.WriteLine(result2);
+            Console.WriteLine(sonuc);
             Console.WriteLine(number1);
 
-            Console.WriteLine(Multiply(2,4));
-            Console.WriteLine(Add4(1, 2, 3, 4, 5, 6));
+            //Console.WriteLine(Multiply(2,4));
+            //Console.WriteLine(Add4(1, 2, 3, 4, 5, 6));
             // ref keywordu değer tip olan bir veri tipini referans olarak göstererek
 
             //Sonucu değiştirmememize yarar. 
@@ -40,19 +42,19 @@
 
         }
 
-        static void Add()
-        {
-            Console.WriteLine("added!!!");
-        }
+        //static void Add()
+        //{
+        //    Console.WriteLine("added!!!");
+        //}
 
-        static int Add2(int number1 = 20, int number2 = 30)
-        {
-            var result = number1 + number2;
-            return result;
-        }
+        //static int Add2(int number1 = 20, int number2 = 30)
+        //{
+        //    var result = number1 + number2;
+        //    return result;
+        //}
 
 
-        static int Add3(ref int number1, int number2)
+        static int Add3( int number1, int number2)
         {
 
             number1 = 30;
@@ -62,19 +64,19 @@
 
         }
 
-        static int Multiply(int number1, int number2)
-        {
-            return number1 * number2;
+        //static int Multiply(int number1, int number2)
+        //{
+        //    return number1 * number2;
 
             
-        }
+        //}
         //ikisinin imzası farklı olduğu için program bana kızmayacak buna OverLoad denir.
-        static int Multiply(int number1, int number2,int number3)
-        {
-            return number1 * number2 * number3;
+       // static int Multiply(int number1, int number2,int number3)
+        //{
+        //    return number1 * number2 * number3;
 
 
-        }
+        //}
 
 
 
@@ -84,10 +86,10 @@
 
     // fonksiyon içerisinde kullanırken return numbers.Sum yazıyoruz.
 
-        static int Add4(params int[] numbers)
-        {
-            return numbers.Sum();
-        }
+        //static int Add4(params int[] numbers)
+        //{
+        //    return numbers.Sum();
+        //}
 
 
        
---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Globalization;
+
+namespace ReferenceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi1 = 10;
+            //int sayi2 = 100;
+
+            //sayi1 = sayi2;
+
+            //sayi2 = 100;
+
+            //Console.WriteLine("Sayı 1 : " + sayi1);
+
+
+
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            person1.FirstName = "Eng";
+
+            person2 = person1;
+            person1.FirstName = "Derinn";
+          Console.WriteLine(person1.FirstName);
+
+            Customer customer = new Customer();
+            customer.FirstName = "Salih";
+            customer.CreditCardNumber = "123567890";
+            
+            Employee employee = new Employee();
+
+            employee.FirstName = "Veli";
+
+            Person person3 = customer;
+
+           customer.FirstName = "Ahmet";
+
+            Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+            PersonManager personManager = new PersonManager(); 
+            personManager.Add(customer);
+
+
+
+        }
+
+
+        class Person // kişi
+        {
+            public int Id { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            
+        }
+        //base class : Person
+        //inheritence : Miras alma 
+        class Customer:Person //Bu bir kişi ama aynı zamanda müşteri //Müşteri
+        {
+            public string CreditCardNumber { get; set; }
+        }
+
+        class Employee:Person // Bu ise çalışan bununda sadece Çalışan numarası var . Ama aynı zamanda bu da bir kişi
+        {
+            public int EmployeeNumber { get; set; }
+        }
+
+        class PersonManager
+        {
+            public void Add(Person person)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+
+    }
+}
