641a8c2389b4ec2d50bc9960d1fb5e7c7bf19248
Murat ahmet Kıroğlu <mrt_drncli4161@outlook.com>
2023-04-29 15:24:01 +0300

    Update

---
+++ Constructors/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id=1,FirstName="Engin",LastName = "Demiroğ",City = "Ankara"};
+            
+            Customer customer2 = new Customer(2,"Derin","Demiroğ","Ankara");
+
+            
+        }
+
+
+        class Customer
+        {
+            //Default Constructor
+            public Customer(int id,string firstname,string lastname,string city)
+            {
+                Console.WriteLine("Yapıcı blok çalıştı");
+            }
+            public int Id { get; set; }
+            
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string City { get; set; }
+
+        }   
+    }
+}
--- ReferenceTypes/Program.cs
+++ ReferenceTypes/Program.cs
@@ -6,44 +6,66 @@ namespace ReferenceTypes
     {
         static void Main(string[] args)
         {
-            //int sayi1 = 10;
-            //int sayi2 = 100;
+            //int,decimal,float,enum,boolean = Value Types
+            int sayi1 = 10; //20
+            int sayi2 = 20;    //100                       
 
-            //sayi1 = sayi2;
+            sayi1 = sayi2 ;
 
-            //sayi2 = 100;
+            sayi2 = 100;
 
-            //Console.WriteLine("Sayı 1 : " + sayi1);
+            Console.WriteLine(sayi1);
+
+
+            int[] sayilar1 = new int[] { 1, 2, 3 };
+            int[] sayilar2 = new int[] { 10, 20, 30 };
+            sayilar1 = sayilar2;
+
+            sayilar2[0] = 1000;
+            
+            Console.WriteLine(sayilar1[0]);
+
 
 
 
             Person person1 = new Person();
             Person person2 = new Person();
 
-            person1.FirstName = "Eng";
+            person1.FirstName = "Engin";
 
-            person2 = person1;
-            person1.FirstName = "Derinn";
-          Console.WriteLine(person1.FirstName);
+           
+            
+            person1 = person2;
+            person1.FirstName = "Derin";
+            Console.WriteLine(person1.FirstName);
 
+            
+            
             Customer customer = new Customer();
             customer.FirstName = "Salih";
             customer.CreditCardNumber = "123567890";
+
+            
+            
             
             Employee employee = new Employee();
-
+            employee.EmployeeNumber = "15";
             employee.FirstName = "Veli";
 
-            Person person3 = customer;
+            
+            
+            
+            Person person3 = customer; 
 
-           customer.FirstName = "Ahmet";
+            customer.FirstName = "Ahmet";
+            
+         //   Console.WriteLine(((Customer)person3).CreditCardNumber);
 
-            Console.WriteLine(((Customer)person3).CreditCardNumber);
-
-            PersonManager personManager = new PersonManager(); 
-            personManager.Add(customer);
+            PersonManager personManager = new PersonManager();
+            personManager.Add(employee);
 
 
+            
 
         }
 
@@ -56,25 +78,25 @@ namespace ReferenceTypes
 
             public string LastName { get; set; }
 
-            
+
         }
         //base class : Person
         //inheritence : Miras alma 
-        class Customer:Person //Bu bir kişi ama aynı zamanda müşteri //Müşteri
+        class Customer : Person //Bu bir kişi ama aynı zamanda müşteri //Müşteri
         {
             public string CreditCardNumber { get; set; }
         }
 
-        class Employee:Person // Bu ise çalışan bununda sadece Çalışan numarası var . Ama aynı zamanda bu da bir kişi
+        class Employee : Person // Bu ise çalışan bununda sadece Çalışan numarası var . Ama aynı zamanda bu da bir kişi
         {
-            public int EmployeeNumber { get; set; }
+            public string EmployeeNumber { get; set; }
         }
 
         class PersonManager
         {
             public void Add(Person person)
             {
-                Console.WriteLine(person.FirstName);
+                Console.WriteLine(person.FirstName + " Added");
             }
         }
 
